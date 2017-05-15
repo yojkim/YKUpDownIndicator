4e608c4218ce94f12a4dbd80e4ccf6a2d0f36c9a
yojkim <youthful2016@gmail.com>
2017-05-16 00:42:46 +0900

    Initial Commit

---
+++ YKUpDownIndicator/YKUpDownIndicator.h
@@ -0,0 +1,19 @@
+//
+//  YKUpDownIndicator.h
+//  YKUpDownIndicator
+//
+//  Created by yojkim on 2017. 5. 16..
+//  Copyright © 2017년 yojkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YKUpDownIndicator.
+FOUNDATION_EXPORT double YKUpDownIndicatorVersionNumber;
+
+//! Project version string for YKUpDownIndicator.
+FOUNDATION_EXPORT const unsigned char YKUpDownIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YKUpDownIndicator/PublicHeader.h>
+
+
